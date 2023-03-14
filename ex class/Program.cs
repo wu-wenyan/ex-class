d6701afdb90b4799eff1b7398567c5e1470bf16b
文彥 <xx48833@gmail.com>
2023-03-14 15:48:54 +0800

    更0314

--- ex class/Program.cs
+++ ex class/Program.cs
@@ -1,10 +1,10 @@
 ﻿void IESort(IEnumerable<int> arr)
 {
-    int[] arry = arr.ToArray();
-    QuickSort(arry, 0, (arry.Length) - 1);
-    IEnumerable<int> array = arry;
-    int j = 0;
-    foreach (int i in array)
+    int[] arry = arr.ToArray(); //轉換為一般陣列去快排
+    QuickSort(arry, 0, (arry.Length) - 1);  //呼叫快排
+    IEnumerable<int> array = arry;  //轉回IEnumerable
+    int j = 0;  
+    foreach (int i in array)    //格式化輸出整理
     {
         if (i < 100)
             Console.Write($"0{i}");
@@ -18,12 +18,12 @@
     }
     return;
 }
-void QuickSort(int[] arr ,int begin ,int end)
+void QuickSort(int[] arr ,int begin ,int end)   //快排1
 {
-    if (begin >= end) return;
+    if (begin >= end) return;   //判斷index
     int priver = QuickSort_one(arr, begin, end);
-    QuickSort(arr, priver + 1, end);
-    QuickSort(arr, begin, priver-1);
+    QuickSort(arr, priver + 1, end);    //左~
+    QuickSort(arr, begin, priver-1);    //右~
 }
 int QuickSort_one(int[] arr,int begin ,int end)
 {
@@ -33,18 +33,14 @@ int QuickSort_one(int[] arr,int begin ,int end)
     while (i < j)
     {
         while (arr[j]>=p && i < j)
-        {
             j--;
-        }
         arr[i] = arr[j];
-        while (arr[i]<=p && i < j)
-        {
+        while (arr[i] <= p && i < j)
             i++;
-        }
         arr[j] = arr[i];
     }
     arr[i] = p;
     return i;
 }
-IEnumerable<int> arr =new[] { 652, 726, 563, 456, 132, 94, 531, 391, 96, 74, 84, 54, 55, 32, 43 };
-IESort(arr);
+IEnumerable<int> arr =new[] { 652, 726, 563, 456, 132, 94, 531, 391, 96, 74, 84, 54, 55, 32, 43 };  //預設數字集合
+IESort(arr);  //呼叫整理集合
